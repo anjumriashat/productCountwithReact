50087e34ac0210bfcffe38369b1a88de00d49c46
anjumriashat <riashat93@yahoo.com>
2019-04-05 16:50:13 +0600

    Initial commit

---
+++ src/componants/counter.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  // Now it's made a controlled (stateless) component to implement 'Single Source of Truth' pattern
+
+  /* a classic approach for 'binding' event handlers */
+  // constructor() {
+  //   super(); // Syntax error: 'this' is not allowed before super()
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  // 'componentDidUpdate' hook of the UPDATEing phase
+  componentDidUpdate(prevProps, prevState) {
+    console.log("Previous Props", prevProps);
+    console.log("Previous States", prevState);
+    if (prevProps.counter.value === this.props.counter.value) {
+      console.log("didn't updated!!!");
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("Component - Unmount");
+  }
+
+  render() {
+    console.log("Counter - Rendered");
+    return (
+      <div className="jumbotron">
+        <h4>Product# {this.props.counter.id}</h4>
+        <button
+          onClick={() => this.props.onDecrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          -
+        </button>
+        <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm ml-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  /* helper methods */
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes +=
+      this.props.counter.value === 0
+        ? "warning"
+        : this.props.counter.value < 0
+        ? "danger"
+        : this.props.counter.value > 0
+        ? "primary"
+        : 0;
+    return classes;
+  }
+  formatCount() {
+    // object destructuring to read the value property of the 'state'
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
